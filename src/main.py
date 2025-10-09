5f1543ac9d100e86d422fe3faeda468c0118d3b8
unknown <dongtrunghuynh@gmail.com>
2025-10-10 00:21:03 +1000

    fixing up main.py

--- src/main.py
+++ src/main.py
@@ -3,10 +3,7 @@ from gui_os import OSSelectionGUI
 from gui_main import FileSystemGUI
 
 def start_app():
-    # Launch OS selection window
-    user_os = OSSelectionGUI().run()  # Returns 'Windows' or 'Mac'
-    
-    # Launch main file system GUI with selected OS
+    user_os = OSSelectionGUI().run()  # Windows or Mac
     FileSystemGUI(user_os).run()
 
 if __name__ == "__main__":
